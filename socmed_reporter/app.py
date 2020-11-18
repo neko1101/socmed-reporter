3aa18c3f7810d4c2377451b514f25f1a2ae1b4cc
tholhahzabri <mat.tempik@gmail.com>
2020-11-18 16:40:27 +0800

    revise print

--- socmed_reporter/app.py
+++ socmed_reporter/app.py
@@ -20,7 +20,7 @@ def check_keyword(site,url):
         ]
 
     for keyword in keywords:
-        if "/{}/".format(keyword) in url:
+        if f"/{keyword}/" in url:
             return True
 
     return False
@@ -36,8 +36,8 @@ def name_clean(name):
     name2 = ""
 
     for keyword in keywords:
-        if " {} ".format(keyword) in name:
-            name = name.split(" {}".format(keyword))   
+        if f" {keyword} " in name:
+            name = name.split(f" {keyword}")   
             soc_med(name[0])    # exclude bin ke atas then just search for name dia
 
     if isinstance(name, list):
@@ -58,18 +58,18 @@ def soc_med(name):
     for site in social_media:
 
         links = []
-        query = "site:{} {}".format(site,name)
+        query = f"site:{site} {name}"
 
         if name == "print_result":
-            print("\n---------- {} ----------".format(site))
+            print(f"\n---------- {site} ----------")
             for x in socmed[f"{site}"]:
                 print(x)
         else:
             for url in search(query, tld="com", num=4, stop=4, pause=0):
-                if not check_keyword("{}".format(site),url):  # go check if the excluded keyword exist in the results
+                if not check_keyword(f"{site}",url):  # go check if the excluded keyword exist in the results
                     link = url.split("/")  # split the links to find which user profile is not redundant in the result
                     if link[3] == "people" and link[4] not in links:
-                        links.append("people/{}".format(link[4]))  # some result has /people/ so need to manually add inside list, add the username if not in the list yet
+                        links.append("people/{link[4]}")  # some result has /people/ so need to manually add inside list, add the username if not in the list yet
                     elif link[3] == "public" and link[4] not in links:
                         links.append(link[4])
                     elif link[3] not in links:
